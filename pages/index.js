64bc54528ffad48f82959acaf00435891e822f01
rostrae <irenewu222@gmail.com>
2022-02-03 15:14:18 -0800

    clear index page

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div style={{textAlign: "center"}}>
-      <h1>Header 1</h1>
-      <h2>Header 2</h2>
-      <h3>Header 3</h3>
-      <h4>Header 4</h4>
-      <h5>Header 5</h5>
-      <h6>Header 6</h6>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p><br/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
-      <p style={{fontFamily: "Arial", fontSize: "16px"}}>Paragraph text</p>
-      
+    <div>
     </div>
   )
 }
