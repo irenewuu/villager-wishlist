8656cd91dd37689617552e6f031efb1169fc4d84
Hyerin <hyerin.bcit@gmail.com>
2022-03-21 15:35:17 -0700

    fixed cursor position

--- comps/VilagersDnd/index.js
+++ comps/VilagersDnd/index.js
@@ -215,7 +215,7 @@ export const VillagersDnd = ({
   }
   if(coords && isDragging){
     console.log(coords, index)
-    sty.left = coords.x +30;
+    sty.left = coords.x +0;
     sty.top = coords.y - 220/2;
     sty.position = 'absolute';
     
